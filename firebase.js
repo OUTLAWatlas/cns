03318cff1087397891dd27686f315dac7b195b09
Krishna Patil <krishna.patil241@vit.edu>
2025-03-29 06:50:04 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,16 +1,26 @@
-import { initializeApp } from "firebase/app";
+
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB5_XfoT1ffzvP7ETtFFjjs1EaFU96_8J0",
-  authDomain: "codenseek-0505.firebaseapp.com",
-  projectId: "codenseek-0505",
-  storageBucket: "codenseek-0505.firebasestorage.app",
-  messagingSenderId: "561075961044",
-  appId: "1:561075961044:web:d46f1691a6da00414e8f41",
-  measurementId: "G-WH613D1BCD"
+  apiKey: "AIzaSyB1_KiNgG-61JqhtkxvJMYA-Uj6NmUag3U",
+  authDomain: "codenseek-f47ef.firebaseapp.com",
+  databaseURL: "https://codenseek-f47ef-default-rtdb.firebaseio.com",
+  projectId: "codenseek-f47ef",
+  storageBucket: "codenseek-f47ef.firebasestorage.app",
+  messagingSenderId: "576347037436",
+  appId: "1:576347037436:web:f68361f578ac571e184179"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-
-console.log("Firebase initialized:", app);
+ 
+        firebase.initializeApp(firebaseConfig);
+ 
+        let teamRef = firebase.database()
+            .ref('Teams');
+ 
+        // Save message to firebase
+        function sameTiming(name, time) {
+            let newTeamRef = teamRef.push();
+            newTeamRef.set({
+                team: name,
+                time: time,
+            });
+        }
